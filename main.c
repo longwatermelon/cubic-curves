325b0214b840878a2ca1dd687629564fdbd5341f
longwatermelon <longwatermelon1@gmail.com>
2022-05-29 22:57:30 -0700

    bezier

--- main.c
+++ main.c
@@ -2,6 +2,25 @@
 #include <stdbool.h>
 #include <SDL2/SDL.h>
 
+SDL_FPoint bezier(SDL_Point points[4], float t)
+{
+    float b[4] = {
+        powf(1 - t, 3.f),
+        3 * t * powf(1 - t, 2.f),
+        3 * powf(t, 2.f) * (1 - t),
+        powf(t, 3.f)
+    };
+
+    SDL_FPoint ret = { 0.f, 0.f };
+
+    for (int i = 0; i < 4; ++i)
+    {
+        ret.x += b[i] * points[i].x;
+        ret.y += b[i] * points[i].y;
+    }
+
+    return ret;
+}
 
 int main(int argc, char **argv)
 {
@@ -11,6 +30,15 @@ int main(int argc, char **argv)
 
     bool running = true;
     SDL_Event evt;
+
+    SDL_Point points[4] = {
+        (SDL_Point){ 100, 400 },
+        (SDL_Point){ 200, 250 },
+        (SDL_Point){ 350, 300 },
+        (SDL_Point){ 500, 600 }
+    };
+
+    SDL_FPoint buf;
 
     while (running)
     {
@@ -25,6 +53,21 @@ int main(int argc, char **argv)
         }
 
         SDL_RenderClear(rend);
+
+        buf = bezier(points, 0.f);
+        SDL_SetRenderDrawColor(rend, 255, 255, 255, 255);
+        for (float i = 0.f; i < 1.f; i += .001f)
+        {
+            SDL_FPoint p1 = bezier(points, i);
+            SDL_RenderDrawLine(rend, p1.x, p1.y, buf.x, buf.y);
+            buf = p1;
+        }
+
+        SDL_SetRenderDrawColor(rend, 255, 0, 0, 255);
+        for (int i = 0; i < 3; ++i)
+        {
+            SDL_RenderDrawLine(rend, points[i].x, points[i].y, points[i + 1].x, points[i + 1].y);
+        }
 
         SDL_SetRenderDrawColor(rend, 0, 0, 0, 255);
         SDL_RenderPresent(rend);
